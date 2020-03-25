a59955d2e8499bfafce5987b18bfd9cf79193c6b
Deyve Vieira <deyvedvm@hotmail.com>
2020-03-24 23:14:10 -0300

    First examples

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+const sayHello = () => "Hello World";
+
+const greet = fnMessage => {
+    console.log(fnMessage());
+};
+
+greet(sayHello);
+
+let numbers = [1, 2, 3, 4];
+
+console.log(numbers);
+
+const sum = numbers.reduce((a, b) => a + b);
+
+console.log(sum);
+
+console.log(numbers.map(number => number * 2));
