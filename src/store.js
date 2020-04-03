d9c17d056e986c63415c1c9e76b39b1f5ba780ef
Deyve Vieira <deyvedvm@hotmail.com>
2020-04-02 22:31:03 -0300

    Using Redux DevTools Extension

--- src/store.js
+++ src/store.js
@@ -1,9 +1,15 @@
 import {createStore} from "redux";
-
-import {devToolsEnhancer} from "redux-devtools-extension";
+import {composeWithDevTools} from "redux-devtools-extension";
 
 import reducer from "./reducer";
 
-const store = createStore(reducer, devToolsEnhancer({trace: true}));
+const composeEnhancers = composeWithDevTools({
+    // Specify name here, actionsBlacklist, actionsCreators and other options if needed
+});
+
+const store = createStore(reducer, /* preloadedState, */ composeEnhancers(
+    // applyMiddleware(...middleware),
+    // other store enhancers if any
+));
 
 export default store;
